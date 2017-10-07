6cbd9b72d87e81b691575403afa4782af3661d50
Neous1 <Neous1@users.noreply.github.com>
2017-10-06 22:45:00 -0400

    min and max number

---
+++ ByteShortInt/src/com/jlVenoy/Main.java
@@ -0,0 +1,10 @@
+package com.jlVenoy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myMinValue = -2147483648;
+        int myMaxValue = 2147483647;
+    }
+}
