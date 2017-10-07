b3c7217ebe7735b5fa5fa5fe6647cda5d5914af1
Neous1 <Neous1@users.noreply.github.com>
2017-10-07 18:58:07 -0400

    primitive data types arithmetic

--- ByteShortInt/src/com/jlVenoy/Main.java
+++ ByteShortInt/src/com/jlVenoy/Main.java
@@ -9,18 +9,27 @@ public class Main {
         int myMaxValue = 2_147_483_647;
         int myTotal = (myMinValue/2);
 
+        System.out.println("myTotal = "+ myTotal +"\n");
+
 
         //byte has a width of 8, ranges from -128 to 127
-        byte myByteValue = 127;
+        byte myByteValue = -128;
         byte myNewByteValue = (byte)(myByteValue/2); // need to cast result to byte to resolve error
+        System.out.println("myNewByteVale = "+myNewByteValue);
 
         //sort has a width of 16, range from -32768 to 32767
         short myShortValue = 32767;
+        short myNewShortValue = (short)(myShortValue/2);
+
+        System.out.println("myNewShortValue =" + myNewShortValue);
 
 
         //long has a width of 64 , ranges from -9_223_372_036_854_775_808 to 9_223_372_036_854_775_807
-        long myLongvalue = 9_223_372_036_854_775_807L;
+        long myLongValue = 9_223_372_036_854_775_807L;
+        long myNewLongValue = (myLongValue/2);
 
-        System.out.println(myMinValue);
+        System.out.println("myNewLongValue = "+ myNewLongValue);
+
+//        System.out.println(myMinValue);
     }
 }
