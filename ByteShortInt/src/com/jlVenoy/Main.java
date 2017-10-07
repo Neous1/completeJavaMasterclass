b84bf4e53bf9a627964aacbc52be32f8f6dfcd67
Neous1 <Neous1@users.noreply.github.com>
2017-10-07 18:35:39 -0400

    working with Byte

--- ByteShortInt/src/com/jlVenoy/Main.java
+++ ByteShortInt/src/com/jlVenoy/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         //byte has a width of 8, ranges from -128 to 127
         byte myByteValue = 127;
-
+        byte myNewByteValue =(myByteValue/2); // need to cast result to byte to resolve error
 
         //sort has a width of 16, range from -32768 to 32767
         short myShortValue = 32767;
