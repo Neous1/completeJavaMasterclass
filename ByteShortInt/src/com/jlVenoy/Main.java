9cd7fd37dfd73d5d79b17a1fa52d97ef5a0d9983
Neous1 <Neous1@users.noreply.github.com>
2017-10-07 18:29:11 -0400

    primitive data types

--- ByteShortInt/src/com/jlVenoy/Main.java
+++ ByteShortInt/src/com/jlVenoy/Main.java
@@ -3,8 +3,22 @@ package com.jlVenoy;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int myMinValue = -2147483648;
-        int myMaxValue = 2147483647;
+
+	// int has a width of 32, the "_" are only meant to make the int more readable.
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+
+        //byte has a width of 8, ranges from -128 to 127
+        byte myByteValue = 127;
+
+
+        //sort has a width of 16, range from -32768 to 32767
+        short myShortValue = 32767;
+
+
+        //long has a width of 64 , ranges from -9_223_372_036_854_775_808 to 9_223_372_036_854_775_807
+        long myLongvalue = 9_223_372_036_854_775_807L;
+
+        System.out.println(myMinValue);
     }
 }
