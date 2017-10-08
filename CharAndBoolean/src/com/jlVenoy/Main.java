d2865f3538f792ea9997c247c9f3e55b39074aaf
Neous1 <Neous1@users.noreply.github.com>
2017-10-07 22:26:01 -0400

    Char and Boolean

---
+++ CharAndBoolean/src/com/jlVenoy/Main.java
@@ -0,0 +1,11 @@
+package com.jlVenoy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char myChar = '\u00A9';
+
+        System.out.println("unicode output was: "+myChar);
+    }
+}
