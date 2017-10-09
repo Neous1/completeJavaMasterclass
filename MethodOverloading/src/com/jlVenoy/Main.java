f71f9c76a34466999cf764403e56014f8c99432b
Neous1 <Neous1@users.noreply.github.com>
2017-10-09 16:30:24 -0400

    method calculateScore get an overload by being declared again and removing one of its parameter to handle the error raised for duplicating the method.

--- MethodOverloading/src/com/jlVenoy/Main.java
+++ MethodOverloading/src/com/jlVenoy/Main.java
@@ -3,12 +3,20 @@ package com.jlVenoy;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("-----------------");
         int newScore = calculateScore("Yvon", 40);
         System.out.println("New score is " + newScore);
+        calculateScore(75);
     }
 
     public static int calculateScore(String playerName, int score){
         System.out.println("Player " +playerName+ " scored "+ score+ " points");
+        return score * 1000;
+    }
+
+    //overload the method mby changing the number of parameters
+    public static int calculateScore(int score){
+        System.out.println("Unnamed player scored "+ score+ " points");
         return score * 1000;
     }
 }
