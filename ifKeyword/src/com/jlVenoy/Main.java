e545221f8cb3b2cfbc242e87ad6c9479c79d7bca
Neous1 <Neous1@users.noreply.github.com>
2017-10-09 03:31:54 -0400

    uses { } with if

---
+++ ifKeyword/src/com/jlVenoy/Main.java
@@ -0,0 +1,16 @@
+package com.jlVenoy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+                if(score==5000){
+                    System.out.println("Your score was 5000");
+                }
+    }
+}
