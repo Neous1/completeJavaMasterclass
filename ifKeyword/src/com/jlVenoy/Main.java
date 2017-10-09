0df536d094e82ba433ad30f2e8070119d043f114
Neous1 <Neous1@users.noreply.github.com>
2017-10-09 11:15:18 -0400

    Challenge time involves duplicating the previous code in order to keep each scored displayed. and that is not good coding practice.

--- ifKeyword/src/com/jlVenoy/Main.java
+++ ifKeyword/src/com/jlVenoy/Main.java
@@ -5,12 +5,51 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         boolean gameOver = true;
-        int score = 5000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
+        System.out.println("---------------");
 
-                if(score==5000)
-                    System.out.println("Your score was 5000");
+//                if(score<5000 && score > 1000){
+//
+//                    System.out.println("Your score was lt 5000 but gt 1000");
+//                }else if (score < 1000){
+//                    System.out.println("Your score was lt 1000");
+//
+//                }else {
+//            System.out.println("Got here");
+//        }
+//
+//
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was "+ finalScore);
+        }
+
+
+
+        System.out.println("------Challenge time---------");
+
+        /*
+        * Print out a second score on the screen with the following
+        * score set to 10000
+        * levelCompleted set to 8
+        * bonus set to 200
+        * but make sure the first printout above still displays as well
+        *
+        * */
+
+         gameOver = true;
+         score = 1000;
+         levelCompleted = 8;
+         bonus = 200;
+
+        if (gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was "+ finalScore);
+        }
+
 
     }
 }
