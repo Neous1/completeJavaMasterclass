821ce60c49a4c144123ea8e01b7aa33e886d746b
Neous1 <Neous1@users.noreply.github.com>
2017-10-08 22:49:55 -0400

    boolean challenge

--- Operators/src/com/jlVenoy/Main.java
+++ Operators/src/com/jlVenoy/Main.java
@@ -69,14 +69,32 @@ public class Main {
             System.out.println("This is true");
 
         boolean isCar = false;
-        if(isCar == true);
-        System.out.println("This is not supposed to happen");
+        if(isCar == true) System.out.println("This is not supposed to happen");
 
 
         isCar = true;
         boolean wasCar = isCar ? true : false;
         if(wasCar)
             System.out.println("wasCar is true");
+
+
+        //Challenge
+
+        //1. Create a double variable with teh value 20
+        //2. Create a second variable of type double with the value 80
+        //3. Add both numbers up adn multiply by 25
+        //4. Use the remainder operator to figure out the remainder from the sun of #3 divided by 40
+        //5. Write an "if' statement that displays a message "Total was over the limit"
+        // if the remaining total (#4) is equal to 20 or less.
+
+
+        System.out.println("-------------------------------------");
+        double newVar = 20d;
+        double secondVAr = 80d;
+
+        double newTotal = (newVar + secondVAr)* 25;
+        System.out.println("the total is : "+newTotal);
+
 
     }
 }
