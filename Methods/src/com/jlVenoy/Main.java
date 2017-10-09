10f8c054d053dc11d612b5611336f18a12da5468
Neous1 <Neous1@users.noreply.github.com>
2017-10-09 13:36:01 -0400

    call calculateScore a second time with different params

--- Methods/src/com/jlVenoy/Main.java
+++ Methods/src/com/jlVenoy/Main.java
@@ -4,35 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score =  800;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
-        System.out.println("---------------");
-
-        calculateScore(gameOver, score, levelCompleted, bonus);
 
 
+        calculateScore(true, 800, levelCompleted, bonus);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        calculateScore(true, 10000, 8, 200);
 
-        if (gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was "+ finalScore);
-        }
+
     }
 
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         System.out.println("---------------");
 
 
-        if (gameOver){
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was "+ finalScore);
+            finalScore += 2000;
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }
