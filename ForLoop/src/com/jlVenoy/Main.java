76143526b24b57d42da97075ea89e3314746ed68
Neous1 <Neous1@users.noreply.github.com>
2017-10-10 11:02:09 -0400

    ForLoop challenge

---
+++ ForLoop/src/com/jlVenoy/Main.java
@@ -0,0 +1,20 @@
+package com.jlVenoy;
+
+public class Main {
+
+    /*
+    * challenge
+    * Using the for statement , call the calculateInterest method with
+    * the amount of 10000 with an interestRate of 2,3,4, 5,6,7 and 8
+    * and print the results to the console window
+    * */
+    public static void main(String[] args) {
+
+        for (int i = 2; i <9; i++)
+        System.out.println("10,000 at "+i+"% interest = " + calculateInterest(10000,i));
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+}
