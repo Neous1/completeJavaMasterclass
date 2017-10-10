3d147bbc031e4148a341a918f6f4c1fa086418e2
Neous1 <Neous1@users.noreply.github.com>
2017-10-10 11:06:10 -0400

    String.format(%,2f) is used to determine the number of digits after the decimal point.

--- ForLoop/src/com/jlVenoy/Main.java
+++ ForLoop/src/com/jlVenoy/Main.java
@@ -11,7 +11,9 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 2; i <9; i++)
-        System.out.println("10,000 at "+i+"% interest = " + calculateInterest(10000,i));
+
+        System.out.println("10,000 at "+i+"% interest = " +
+                String.format("%.2f", calculateInterest(10000,i)));
     }
 
     public static double calculateInterest(double amount, double interestRate) {
