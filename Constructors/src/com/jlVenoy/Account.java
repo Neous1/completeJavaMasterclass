f26890a88f810153912400915841b717da411ea4
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 12:31:51 -0400

    create the Account class

---
+++ Constructors/src/com/jlVenoy/Account.java
@@ -0,0 +1,9 @@
+package com.jlVenoy;
+
+public class Account {
+    private String number;
+    private double balaance;
+    private String customerNmae;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+}
