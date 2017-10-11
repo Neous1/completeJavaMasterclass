a901e1090a6d85d93a5db21b8da9ca7cff3612ed
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 09:10:44 -0400

    get the count of even numbers

--- While/src/com/jlVenoy/Main.java
+++ While/src/com/jlVenoy/Main.java
@@ -32,10 +32,12 @@ public class Main {
 //    }
         //challenges
         /*Create a method called isEvenNumber that takes a parameter of type int
-        * Its purpose s to determine if the argument passed to the method is an even number or not .
+        * Its purpose s to determine if the argument passed to the method is
+        * an even number or not .
         * return true if a even number , otherwise return false
         * */
 
+        int count = 0;
         int number = 5;
         int finishNumber = 20;
         while(number <= finishNumber){
@@ -43,9 +45,18 @@ public class Main {
                 number++;
                 continue;
             }
-            System.out.println("Even number "+number);
+count++;
+            System.out.println("Even number "+number );
             number++;
         }
+        System.out.println("total number of even number  = " + count);
+
+
+        /* Modify the while code above
+        make it also record the total number of even numbers it has found
+        adn break once 5 are found
+        and at the end, display the total number of even numbers found
+         */
     }
 
 
