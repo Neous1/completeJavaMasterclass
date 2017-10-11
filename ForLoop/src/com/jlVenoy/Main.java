aafb3e4ffb2efc387dc5928f8a23eece383d25d9
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 06:03:32 -0400

    add code for finding prime number

--- ForLoop/src/com/jlVenoy/Main.java
+++ ForLoop/src/com/jlVenoy/Main.java
@@ -25,12 +25,40 @@ public class Main {
 
             System.out.println("Reverse printout : 10,000 at "+i+"% interest = " +
                     String.format("%.2f", calculateInterest(10000,i)));
+
+
+        int count = 0;
+        for (int i =10; i<50; i++){
+            if(isPrime(i)){
+                count++;
+                System.out.println("Number "+i+" is a prime number");
+                if(count ==10){
+                    System.out.println("Exiting For loop");
+                    break;
+                }
+            }
+//            System.out.println("is "+i+" prime ?" + isPrime(10));
+        }
+
     }
+
 
 
 
 
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
+    }
+
+    public static boolean isPrime(int n){
+        if(n ==1){
+            return false;
+        }
+        for (int i = 2; i<= n/2; i++){
+            if (n % i ==0 ){
+                return false;
+            }
+        }
+        return true;
     }
 }
