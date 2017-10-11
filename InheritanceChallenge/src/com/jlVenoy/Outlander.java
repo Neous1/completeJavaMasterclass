bebdf22a1a119da25d62b8e660bb1e6211c0a5f3
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 18:33:47 -0400

    Create a outlander class which inherits from Car which inherits from Vehicle

---
+++ InheritanceChallenge/src/com/jlVenoy/Outlander.java
@@ -0,0 +1,10 @@
+package com.jlVenoy;
+
+public class Outlander extends Car {
+    private int roadServiceMonths;
+
+    public Outlander(String name, String size, int wheels, int doors, int gears, boolean isManual, int roadServiceMonths) {
+        super(name, size, wheels, doors, gears, isManual);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+}
