ebdfbaf4f0bc89bebaa81b44b2ce77a28f67bb7a
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 10:29:04 -0400

    create a car class and its states

---
+++ classes/src/com/jlVenoy/Car.java
@@ -0,0 +1,11 @@
+package com.jlVenoy;
+
+public class Car {
+    //state of the class
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+}
---
+++ classes/src/com/jlVenoy/Main.java
@@ -0,0 +1,12 @@
+package com.jlVenoy;
+
+public class Main {
+
+    public static void main(String[] args) {
+     //create an object
+     Car porshe = new Car();
+     Car holden = new Car();
+
+
+    }
+}
