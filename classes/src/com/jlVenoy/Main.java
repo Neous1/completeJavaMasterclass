0c8ec56fb03831c6cb16e7568a6254c2823f3360
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 12:06:38 -0400

    add a getModel method to car class to be called on main class in log

--- classes/src/com/jlVenoy/Main.java
+++ classes/src/com/jlVenoy/Main.java
@@ -7,5 +7,6 @@ public class Main {
      Car porsche = new Car();
      Car holden = new Car();
      porsche.setModel("Carrera");
+        System.out.println("Model is "+porsche.getModel());
     }
 }
