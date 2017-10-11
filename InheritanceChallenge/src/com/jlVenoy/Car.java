cb07b98e4c86ffbb55fe26adefa8470a14fe1903
Neous1 <Neous1@users.noreply.github.com>
2017-10-11 19:42:54 -0400

    hardcode outlander's field and remove them from the car's parameters add move() for readibility

--- InheritanceChallenge/src/com/jlVenoy/Car.java
+++ InheritanceChallenge/src/com/jlVenoy/Car.java
@@ -20,12 +20,12 @@ public class Car extends Vehicle {
     //changeGear sounds better that setcurrentGear
     public void changeGear(int currentGear) {
         this.currentGear = currentGear;
-        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear+" gear");
+        System.out.println("Car.setCurrentGear(): Changed to gear # " + this.currentGear);
     }
 
     public void changeVelocity(int speed, int direction){
-        move(speed, direction);
         System.out.println("Car.changeVelocity() : Velocity " + speed + " direction " + direction);
+        move(speed, direction);
     }
 
 
