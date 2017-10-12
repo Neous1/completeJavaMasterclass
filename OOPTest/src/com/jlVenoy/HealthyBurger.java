cacb591dbb8bf277d8ba3c0dddd309c2eb5b4c83
Neous1 <Neous1@users.noreply.github.com>
2017-10-12 05:02:35 -0400

    create a HealthyBurger class

---
+++ OOPTest/src/com/jlVenoy/HealthyBurger.java
@@ -0,0 +1,29 @@
+package com.jlVenoy;
+
+public class HealthyBurger extends Hamburger {
+    // a) Healthy burger (on a brown rye bread roll), plus two addition items can be added.
+    // The healthy burger can have a total of 6 items (Additions) in total.
+    // hint:  you probably want to process the 2 additional items in this new class, not the base class,
+    // since the 2 additions are only appropriate for this new class.
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    //we only need the meat and the price
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy", meat, price, "Brown Rye");
+    }
+
+    public void addHealthAddition1(String name, double price){
+        this.healthyExtra1Name = name;
+        this. healthyExtra1Price = price;
+    }
+
+    public void addHealthAddition2(String name, double price){
+        this.healthyExtra1Name = name;
+        this. healthyExtra1Price = price;
+    }
+}
