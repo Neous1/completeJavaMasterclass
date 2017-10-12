2c06615cda6f6d6132864af868f8d44d400db37e
Neous1 <Neous1@users.noreply.github.com>
2017-10-12 04:12:00 -0400

    create methods to add the 4 additional ingredients

--- OOPTest/src/com/jlVenoy/Hamburger.java
+++ OOPTest/src/com/jlVenoy/Hamburger.java
@@ -26,4 +26,27 @@ public class Hamburger {
         this.price = price;
         this.breadRollType = breadRollType;
     }
+
+    //Track the additional ingredients
+
+    public void addHamburgerAddition1(String name, double price){
+        this.addition1Name = name;
+        this.addition1Price = price;
+    }
+    public void addHamburgerAddition2(String name, double price){
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price){
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price){
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
+
+
 }
