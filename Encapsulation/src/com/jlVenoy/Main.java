d91266a1a4fbaf8fb90ab0f037368ffd44f234cc
Neous1 <Neous1@users.noreply.github.com>
2017-10-12 00:15:20 -0400

    change health to hitpoints, to demo Encpasulation

--- Encapsulation/src/com/jlVenoy/Main.java
+++ Encapsulation/src/com/jlVenoy/Main.java
@@ -19,7 +19,7 @@ public class Main {
 //		System.out.println("Remaining health = " + player.healthRemaining());
 
 
-		EnhancedPlayer player = new EnhancedPlayer("Yvon", 75, "numchuck");
+		EnhancedPlayer player = new EnhancedPlayer("Yvon", 200, "numchuck");
 		System.out.println("Initial health is " + player.getHealth());
 	}
 }
