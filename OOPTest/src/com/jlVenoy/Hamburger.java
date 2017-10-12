75336a8a89d0f8433fff505ec651eea3000c9656
Neous1 <Neous1@users.noreply.github.com>
2017-10-12 03:59:11 -0400

    keep track of the optional topping

--- OOPTest/src/com/jlVenoy/Hamburger.java
+++ OOPTest/src/com/jlVenoy/Hamburger.java
@@ -6,6 +6,19 @@ public class Hamburger {
     private double price;
     private String breadRollType;
 
+//track additional ingredient to burger and their cost
+    private String addition1Name;
+    private double addition1Price;
+
+    private String addition2Name;
+    private double addition2Price;
+
+    private String addition3Name;
+    private double addition3Price;
+
+    private String addition4Name;
+    private double addition4Price;
+
 
     public Hamburger(String name, String meat, double price, String breadRollType) {
         this.name = name;
