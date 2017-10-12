66dd8eb5af7abbe169276891870ed8aad29c7265
Neous1 <Neous1@users.noreply.github.com>
2017-10-12 03:57:20 -0400

    create Hamberger class adn its constructor

---
+++ OOPTest/src/com/jlVenoy/Hamburger.java
@@ -0,0 +1,16 @@
+package com.jlVenoy;
+
+public class Hamburger {
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+}
